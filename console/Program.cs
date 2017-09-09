af1238f5ceceeffe24a95b62091395bb4a5fd534
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-09-08 21:34:40 -0300

    criação do projeto console

---
+++ console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using lib;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Calculadora calc = new Calculadora();
+            int r = calc.Soma(2,3);
+            Console.WriteLine(r);
+        }
+    }
+}
